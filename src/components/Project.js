8633be27e543300d6aa74ef7252e7ea16c7cc40e
alex barlow <alex.barlow@rocketmail.com>
2023-01-25 15:01:45 -0500

    added header, navbar and projects component

---
+++ src/components/Project.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/Project.css';
+
+function Project() {
+    return (
+        <section className='section'>
+            <h2>Portfolio Projects</h2>
+        </section>
+    )
+}
