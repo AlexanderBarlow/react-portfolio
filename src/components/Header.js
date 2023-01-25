35095fd56c4a02dad91580e9c7f58cdb9b9500ae
alex barlow <alex.barlow@rocketmail.com>
2023-01-25 15:27:02 -0500

    Setup Header.js to handle page changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,37 @@
 import React, { useState } from 'react';
 import Navigation from './Navigation';
-import 
-
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Portfolio from './pages/About';
+import Resume from  './pages/Resume';
 import '../styles/Header.css';
 
-function Header() {
+//React.Activity24
+export default function Header() {
+    const [currentPage, setCurrentPage] = useState('About');
+
+    const renderPage = () => {
+        if(currentPage === 'About') {
+            return <About />;
+        }
+        if (currentPage === 'Contact') {
+            return <Contact />;
+        }
+        if (currentPage === 'Portfolio') {
+            return <Portfolio />;
+        }
+        else {
+            return <Resume />;
+        }
+    }
+
+    const handlePageChange = (page) => setCurrentPage(page);
+    
     return (
-        <header className='header'>
-            <h1>Alexander Barlow</h1>
-        </header>
+        <div>
+                <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+                {renderPage()}
+        </div>
     );
 }
 
-export default Header;
