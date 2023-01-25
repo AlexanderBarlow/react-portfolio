5e96b3370a2017e0391ad8039c3784f2c8ddb6bc
alex barlow <alex.barlow@rocketmail.com>
2023-01-25 17:09:52 -0500

    made the footer in footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,9 @@ import "../styles/Footer.css"
 function Footer() {
     return (
         <footer className='footer'>
-            <img></img>
+            <a href='https://github.com/alexanderbarlow' target='_blank'><i class='fa-brands fa-github'></i></a>
+            <a href='https://www.linkedin.com/in/alexanderlbarlow/' target='_blank'><i class='fab fa-linkedin-in'></i></a>
+            <a href='mailto:alexanderbarlow7@gmail.com'><i class='fa fa-envolope' aria-hidden='true'></i></a>
         </footer>
     )
 }
